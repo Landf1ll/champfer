16d2e53debecd4c77a918ac77f5162c2344f6047
Thomas Vine <tommyvine@gmail.com>
2016-07-13 16:06:18 +0100

    Initial Commit

---
+++ Champfer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Champfer
+//
+//  Created by Thomas Vine on 13/07/2016.
+//  Copyright © 2016 Thomas Vine. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
