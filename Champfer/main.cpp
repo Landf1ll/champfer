6d08bb46cfc1a20dc78e9aefc552c1b41be34538
Thomas Vine <tommyvine@gmail.com>
2016-07-13 16:08:49 +0100

    started project development

--- Champfer/main.cpp
+++ Champfer/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    cout << "Hello, World!" << endl;
+    
     return 0;
 }
